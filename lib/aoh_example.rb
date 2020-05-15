ff5d9c8d8b8f4fa56b1757ff209d8efad966c1ff
Orlando Challenger <orlandofat@gmail.com>
2020-05-15 00:02:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -27,6 +27,15 @@ end
 
 def aoh_lookup(aoh, row, key)
   aoh_lookup
+  
+  job =[
+         { name:  "Don Gately", occupation: "Live-in Staff" },
+         { name:  "Joelle van Dyne", occupation: "Radio Personality" }, 
+         { name:  "Pat Monteseian", occupation: "Staff" },
+         { name:  "Kate Gompert", occupation: "None" },
+         { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    ]
+    job
 end
 
 def aoh_update(aoh, row, key, new_value)
